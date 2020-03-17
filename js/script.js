ba270897342aa38e36fc0793ef75ad8849d7c6b3
Damian Jackiewicz <damian.jackiewicz92@gmail.com>
2020-03-17 19:55:25 +0100

    Add about me and technologies section

--- js/script.js
+++ js/script.js
@@ -8,3 +8,33 @@ const hamburgerClick = () => {
 }
 
 hamburger.addEventListener('click', hamburgerClick);
+
+// SMOOTH SCROLL
+scrollTo = (element) => {
+    window.scroll({
+        behavior: 'smooth',
+        left: 0,
+        top: element.offsetTop,
+    })
+
+}
+
+document.querySelector(".menu__item--home").addEventListener('click', () => {
+    scrollTo(document.getElementById("home"));
+})
+
+document.querySelector(".menu__item--about").addEventListener('click', () => {
+    scrollTo(document.getElementById("about"));
+})
+
+document.querySelector(".menu__item--technologies").addEventListener('click', () => {
+    scrollTo(document.getElementById("technologies"));
+})
+
+document.querySelector(".menu__item--projects").addEventListener('click', () => {
+    scrollTo(document.getElementById("projects"));
+})
+
+document.querySelector(".menu__item--contact").addEventListener('click', () => {
+    scrollTo(document.getElementById("contact"));
+})
